28262cfd419b4b1c5e113732a749c9491c7335da
Allison <allisonsouza10261@gmail.com>
2025-09-05 07:43:24 -0300

    feat(entity): add User entity with attributes name, email, password, role

---
+++ src/main/java/com/br/eventmanagement/entity/User.java
@@ -0,0 +1,40 @@
+package com.br.eventmanagement.entity;
+
+import com.br.eventmanagement.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<Registration> registrations;
+
+}
---
+++ src/main/java/com/br/eventmanagement/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.br.eventmanagement.enums;
+
+public enum UserRole {
+    ADMIN, PARTICIPANT
+}
