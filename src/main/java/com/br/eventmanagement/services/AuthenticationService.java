c1a98dd17b98e36a9d15ae5bc224b37740d354d1
Allison <allisonsouza10261@gmail.com>
2025-10-03 08:31:15 -0300

    feat:(Service) creating AuthenticationService with login and register users methods

---
+++ src/main/java/com/br/eventmanagement/services/AuthenticationService.java
@@ -0,0 +1,73 @@
+package com.br.eventmanagement.services;
+
+import com.br.eventmanagement.dtos.AuthenticationDto;
+import com.br.eventmanagement.dtos.RegisterDto;
+import com.br.eventmanagement.entity.User;
+import com.br.eventmanagement.enums.UserRole;
+import com.br.eventmanagement.repositories.UserRepository;
+import com.br.eventmanagement.security.TokenService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final TokenService tokenService;
+
+    //using lazy here, to the AuthenticationManager do not get in loop with the same from SecurityConfiguration
+    public AuthenticationService(@Lazy AuthenticationManager authenticationManager,
+                                 UserRepository userRepository, TokenService tokenService){
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+
+/*
+* When you use the authenticate the spring already do the entire process of verification
+ going to the database and making the verification between the data typed and the data stored.
+
+* Basically the PasswordEncoder method in SecurityConfig does the verification of the hash between passwords in the
+    authentication when you call the authenticate, like below
+
+* put try-catch in the exception threw by this method "authenticate"
+ */
+    public String login(AuthenticationDto authDto){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(authDto.username(), authDto.password());
+        try{
+            var auth = authenticationManager.authenticate(usernamePassword);
+            return tokenService.generateToken((User) auth.getPrincipal());
+
+        } catch (AuthenticationException e){
+            throw new RuntimeException("Error while try to authenticate"); //I will personalize it later...
+        }
+    }
+
+    @Transactional
+    public User register(RegisterDto registerDto){
+        if(userRepository.findByUsername(registerDto.username())!= null) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDto.password());
+
+        User newUser = new User(registerDto.username(), encryptedPassword, registerDto.email());
+        newUser.setRole(UserRole.PARTICIPANT);
+        return userRepository.save(newUser);
+    }
+
+}
