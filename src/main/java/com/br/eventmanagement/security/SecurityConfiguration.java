263e0fa71eb1b9e7656eb7772ee6b73e0d3fe119
Allison <allisonsouza10261@gmail.com>
2026-01-05 11:03:48 -0300

    feat(Security, Config): Putting out the PasswordEncoder bean from SecurityConfiguration to avoid cicle loop inside of UserService.

--- src/main/java/com/br/eventmanagement/security/SecurityConfiguration.java
+++ src/main/java/com/br/eventmanagement/security/SecurityConfiguration.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -46,10 +44,5 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
